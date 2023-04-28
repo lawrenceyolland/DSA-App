400c30fc16ecd26cacd2080ec1174ababebdc8bd
Lawrence Yolland <lawrence.yolland@ericsson.com>
2023-04-28 16:06:33 +0100

    add async sort and restyled bars

--- src/app.tsx
+++ src/app.tsx
@@ -1,42 +1,92 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { createRoot } from "react-dom/client";
 import { linearSearch, binarySearch } from "./utils";
+import { hydrate } from "react-dom";
+
+type Haystack = (number | string)[][];
+
+const ARRAY_SIZE = 10;
+const NEEDLE = 3;
+const TIME_TO_WAIT = 50;
+
+const generateHaystackArray = (): Haystack => {
+  return [...Array(ARRAY_SIZE)].map((_, idx) => [
+    idx,
+    Math.floor(Math.random() * ARRAY_SIZE),
+    `#${Math.floor(Math.random() * 16777215).toString(16)}`,
+  ]);
+};
 
 function App() {
   const [counter, setCounter] = useState<number>(0);
-  const [needle, setNeedle] = useState<number>(3);
+  const [needle, _] = useState<number>(NEEDLE);
   const [result, setResult] = useState<string>("");
-  const [haystack, setHaystack] = useState<number[]>([1, 2, 3, 4, 5]);
-  const [searchType, setSearchType] = useState<string>("Linear Search");
+  const [haystack, setHaystack] = useState<Haystack>(generateHaystackArray());
+  const [type, setType] = useState<string>("sort");
+  const [title, setTitle] = useState<string>("Bubble Sort");
+  const [sorted, setSorted] = useState<Haystack>(haystack);
+  const [disableSort, setDisableSort] = useState<boolean>(false);
 
-  const handleSearch = (): void => {
-    switch (searchType) {
-      case "Linear Search":
-        linearSearch(haystack, needle)
-          ? setResult(`${needle} found in array`)
-          : setResult(`${needle} not found in array`);
-        break;
-      case "Binary Search":
-        binarySearch(haystack, needle)
-          ? setResult(`${needle} found in array`)
-          : setResult(`${needle} not found in array`);
-        break;
-      default:
-        break;
-    }
+  const waitToResolve = (): Promise<void> => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, TIME_TO_WAIT);
+    });
+  };
+
+  async function bubbleSort(haystack: Haystack): Promise<void> {
+    setDisableSort(true);
+
+    const promises: Promise<TimerHandler>[] = [];
+
+    let arr = [...haystack];
+    for (let i = 0; i < arr.length; i++)
+      for (let j = 0; j < arr.length - 1 - i; j++) {
+        if (arr[j][1] > arr[j + 1][1]) {
+          let tmp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = tmp;
+          setSorted(arr.slice());
+          setCounter((prev) => prev + 1);
+          await waitToResolve();
+        }
+      }
+    setResult("Sorted");
+    setDisableSort(false);
+  }
+
+  const handleRegenerateHaystack = (): void => {
+    const newHaystack = generateHaystackArray();
+    setHaystack(newHaystack);
+    setSorted(newHaystack);
+    setCounter(0);
   };
 
   return (
     <section>
       <div className="search-container">
-        <h1>{searchType}</h1>
-        <button type="button" onClick={handleSearch}>
-          Search
-        </button>
+        <h1>{title}</h1>
+        <div className="action-container">
+          <button
+            type="button"
+            disabled={disableSort}
+            onClick={() => bubbleSort(haystack)}
+          >
+            Sort
+          </button>
+          <button
+            type="button"
+            disabled={disableSort}
+            onClick={handleRegenerateHaystack}
+          >
+            Reset
+          </button>
+        </div>
         <div className="info-panel">
-          <div>
-            Looking for: <span id="needle">{needle}</span>
-          </div>
+          {type === "search" && (
+            <div>
+              Looking for: <span id="needle">{needle}</span>
+            </div>
+          )}
           <div>
             Iterations: <span id="iter-counter">{counter}</span>
           </div>
@@ -45,10 +95,15 @@ function App() {
           </div>
         </div>
         <div id="array-container">
-          {haystack.map((item) => (
-            <div key={item} className="list-item">
-              {item}
-            </div>
+          {sorted?.map((i) => (
+            <div
+              style={{
+                height: `${(i[1] as number) * 10}px`,
+                backgroundColor: `${i[2]}`,
+              }}
+              className="list-item"
+              key={i[0]}
+            />
           ))}
         </div>
       </div>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
-export const linearSearch = (haystack: number[], needle: number): boolean => {
+
+export const linearSearch = (
+  haystack: Array<string | number>,
+  needle: string | number
+): boolean => {
   for (let i = 0; i < haystack.length; i++) {
     if (haystack[i] === needle) {
       return true;
@@ -7,13 +11,16 @@ export const linearSearch = (haystack: number[], needle: number): boolean => {
   return false;
 };
 
-export const binarySearch = (haystack: number[], needle: number): boolean => {
+export const binarySearch = (
+  haystack: Array<string | number>,
+  needle: string | number
+): boolean => {
   let start: number = 0;
   let end: number = haystack.length;
 
   do {
     let midPoint: number = Math.floor(start + (end - start) / 2);
-    let midValue: number = haystack[midPoint];
+    let midValue = haystack[midPoint];
 
     if (midValue === needle) {
       return true;
